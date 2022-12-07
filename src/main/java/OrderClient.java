8c75ca7119dbe768efbffb924f0b85055f7e3b5d
Ilya Goryachenkov <i-ama-i@ya.ru>
2022-12-07 13:59:34 +0300

    Add OrderClient

---
+++ src/main/java/OrderClient.java
@@ -0,0 +1,33 @@
+import io.qameta.allure.Step;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.given;
+
+public class OrderClient extends Client{
+
+    private static final String PATH_ORDER = "api/orders";
+
+    @Step("Создание заказа")
+    public ValidatableResponse createOrder(Ingredients ingredients) {
+        return given()
+                .spec(getSpec())
+                .log().all()
+                .body(ingredients)
+                .when()
+                .post(PATH_ORDER)
+                .then()
+                .log().all();
+    }
+
+    @Step("Создание заказа")
+    public ValidatableResponse getUserOrders(Ingredients ingredients) {
+        return given()
+                .spec(getSpec())
+                .log().all()
+                .body(ingredients)
+                .when()
+                .get(PATH_ORDER)
+                .then()
+                .log().all();
+    }
+}
